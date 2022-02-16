664d532ef972e6ed02856e7f9ae14547f60b15fb
rosemutai <chepngetichrose2030@gmail.com>
2022-02-16 11:28:01 +0300

    app vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="nav">
+    <NavBar/>
+     <!-- <hr> -->
+   <router-view/>
+   <br>
+   <br>
+   <br>
+    <FooterNav/>
+   
+    
+  </div>
+  
+</template>
+
+<script>
+
+import NavBar from './components/NavBar.vue'
+import FooterNav from './components/FooterNav.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    FooterNav
+  }
+}
+</script>
+
+
+
+
+
+
+
+
+<style>
+
+#app {
+  max-width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+ 
+  /* #D4A373; */
+  font-family: 'Roboto', sans-serif;
+  
+}
+
+hr{
+  border: none;
+  border-bottom: 1px solid #fbac0a;
+  max-width: 90%;
+  margin: 0 auto;
+}
+
+
+</style>
